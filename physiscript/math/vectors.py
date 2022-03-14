8b0cca24aec3bca2fb3e26a116bfbf9537db78a7
Shai Avraham <shaia1112011@gmail.com>
2022-03-14 12:03:17 +0200

    Started developing the Vector class

--- physiscript/math/vectors.py
+++ physiscript/math/vectors.py
@@ -1,5 +1,32 @@
 """vectors."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+    import numpy.typing as npt
+
+    from physiscript.typing import FloatScalar
 
 
 class Vector:
-    """Vector."""
+    """A multi-dimensional `Vector` class."""
+
+    __slots__ = ("_data",)
+
+    _data: npt.NDArray[np.float64]
+
+    def __init__(self, coordinates: Iterable[FloatScalar]) -> None:
+        try:
+            count = len(coordinates)  # type: ignore[arg-type]
+        except TypeError:
+            count = -1
+        self._data = np.fromiter(coordinates, count=count, dtype=np.float64)
+
+    @classmethod
+    def of(cls, *args: FloatScalar) -> Vector:
+        return Vector(args)
---
+++ physiscript/typing.py
@@ -0,0 +1,5 @@
+from typing import SupportsComplex, SupportsFloat, SupportsInt
+
+IntScalar = SupportsInt | str
+FloatScalar = SupportsFloat | str
+ComplexScalar = SupportsComplex | str
