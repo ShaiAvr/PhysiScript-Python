8f36076e843ce9e0771b478c5b8d14781a4d86b4
Shai Avraham <shaia1112011@gmail.com>
2022-03-19 23:42:22 +0200

    Added pyupgrade pre-commit hook

--- physiscript/math/vectors.py
+++ physiscript/math/vectors.py
@@ -1,4 +1,15 @@
-"""vectors."""
+"""A multi-dimensional `Vector` class.
+
+A `Vector` is built from an iterable of numbers or given numbers::
+
+    >>> Vector([1, 2, 3])
+    Vector([1.0, 2.0, 3.0])
+    >>> Vector.of(0.0, -2.5)
+    Vector([0.0, -2.5])
+    >>> Vector(range(10)) # doctest: +ELLIPSIS
+    Vector([0.0, 1.0, 2.0, 3.0, ..., 9.0])
+
+"""
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
@@ -6,7 +17,7 @@ from typing import TYPE_CHECKING
 import numpy as np
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
+    from collections.abc import Iterable, Iterator
 
     import numpy.typing as npt
 
@@ -30,3 +41,20 @@ class Vector:
     @classmethod
     def of(cls, *args: FloatScalar) -> Vector:
         return Vector(args)
+
+    def __iter__(self) -> Iterator[float]:
+        return self._data.flat  # type: ignore[return-value]
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}([{', '.join(map(repr, self))}])"
+
+    def __str__(self) -> str:
+        return f"<{', '.join(map(str, self))}>"
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Vector):
+            return np.array_equal(self._data, other._data, equal_nan=True)
+        return NotImplemented
+
+    def __abs__(self) -> float:
+        return np.sqrt(self._data.dot(self._data))
